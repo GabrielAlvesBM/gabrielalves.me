feb4e0114066459f2c76839ce942b6778e3a2dac
gabrielalvesbm <gabrielalves6002@gmail.com>
2024-09-16 16:36:03 -0300

    projetos.index v.0.4

--- portifolio/assets/scripts/projetos.js
+++ portifolio/assets/scripts/projetos.js
@@ -20,19 +20,61 @@ const sidebarModal = document.querySelector(".sidebar-modal");
 
 const projetos = document.querySelectorAll(".projeto");
 
+const sidebar = {
+    title:               document.querySelector("#sidebar__title"),
+    shortDescription:    document.querySelector("#sidebar__short-description"),
+    image:               document.querySelector("#sidebar__image"),
+    longDescription:     document.querySelector("#sidebar__long-description"),
+    tecList:             document.querySelector("#sidebar__tec-list"),
+    site:                document.querySelector("#sidebar__site"),
+    github:              document.querySelector("#sidebar__github"),
+    openProject:         document.querySelector("#open-project"),
+};
 
-// Fechar o Modal
 function closeModal() {
     sidebarModal.classList.remove("actived");
     body.classList.remove("none-overflow");
     backdrop.classList.remove("actived");
-}
+
+    const selectedProjeto = document.querySelector(".projeto.selected");
+    selectedProjeto.classList.remove("selected");
+};
 
 function openModal() {
     sidebarModal.classList.add("actived");
     body.classList.add("none-overflow");
     backdrop.classList.add("actived");
-}
+};
+
+function updateSidebar(projeto) {
+    const dataAttributes = {
+        title:               projeto.getAttribute("data-title"),
+        shortDescription:    projeto.getAttribute("data-short-description"),
+        image:               projeto.getAttribute("data-image"),
+        longDescription:     projeto.getAttribute("data-long-description"),
+        tecList:             projeto.getAttribute("data-tec-list").split(','),
+        site:                projeto.getAttribute("data-site"),
+        github:              projeto.getAttribute("data-github"),
+    };
+
+    sidebar.title.textContent = dataAttributes.title;
+    sidebar.shortDescription.textContent = dataAttributes.shortDescription;
+    sidebar.image.src = dataAttributes.image;
+    sidebar.longDescription.textContent = dataAttributes.longDescription;
+    sidebar.tecList.textContent = dataAttributes.tecList;
+    sidebar.site.href = dataAttributes.site;
+    sidebar.site.textContent = dataAttributes.site;
+    sidebar.github.href = dataAttributes.github;
+    sidebar.github.textContent = dataAttributes.github;
+    sidebar.openProject.href = dataAttributes.site;
+
+    sidebar.tecList.innerHTML = '';
+    for (let item of dataAttributes.tecList) {
+        let li = document.createElement("li");
+        li.textContent = item;
+        sidebar.tecList.appendChild(li);
+    }
+};
 
 closeAside.addEventListener("click", closeModal);
 backdrop.addEventListener("click", closeModal);
@@ -44,6 +86,11 @@ backProject.addEventListener("click", (event) => {
 // Abrir o Modal
 
 projetos.forEach((projeto) => {
-    projeto.addEventListener("click", openModal);
+    projeto.addEventListener("click", () => {
+        projeto.classList.add("selected");
+
+        updateSidebar(projeto);
+        openModal();
+    });
 });
 
