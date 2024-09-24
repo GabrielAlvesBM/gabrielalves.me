a2c79c1f0e5bd2c94c545059c285ab4f92417ea2
Gabriel Alves <gabrielalves6002@gmail.com>
2024-09-24 14:55:17 -0300

    enviarEmail.js finished

--- src/assets/scripts/enviarEmail.js
+++ src/assets/scripts/enviarEmail.js
@@ -1,71 +1,42 @@
-class FormSubmit {
-    constructor(settings) {
-        this.settings = settings;
-        this.form = document.querySelector(settings.form);
-        this.formButton = document.querySelector(settings.button);
-        if (this.form) {
-            this.url = this.form.getAttribute("action");
-        }
-        this.sendForm = this.sendForm.bind(this);
-    };
+const form = document.querySelector("form");
+const submitBtn = document.querySelector(".btn");
 
-    displaySuccess() {
-        this.form.innerHTML = this.settings.success;
-    }
-
-    displayError() {
-        this.form.innerHTML = this.settings.error;
-    }
-
-    getFormObject() {
-        const formObject = {};
-        const fields = this.form.querySelectorAll("[name]");
-        fields.forEach((field) => {
-            formObject[field.getAttribute("name")] = field.value;
-        });
-        return formObject;
-    }
-
-    onSubmission(event) {
-        event.preventDefault();
-        event.target.disabled = true;
-        event.target.innerText = "Enviando..."
-        event.target.classList.add("btnEnviando");
-    }
-
-    async sendForm(event) {
-        if (!this.form.checkValidity()) {
-            return;
-        }
-
-        try {
-            this.onSubmission(event);
-            await fetch(this.url, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: JSON.stringify(this.getFormObject()),
-            });
-            this.displaySuccess();
-        } catch(error) {
-            this.displayError();
-            this.reEnableButton(event.target);
-            throw new Error(error);
-        }
-    }
-
-    init() {
-        if (this.form) this.formButton.addEventListener("click", this.sendForm);
-        return this;
-    }
+function onSubmission(event) {
+    event.disabled = true;
+    event.innerText = "Enviando..."
+    event.classList.add("btnEnviando");
 };
 
-const formSubmit = new FormSubmit({
-    form: "[data-form]",
-    button: "[data-button]",
-    success: "<h1 class='title enviado'>Mensagem Enviada!</h1> <p>Fico feliz com o seu contato e retornarei em breve.</p>",
-    error: "<h1 class='title enviado'>Mensagem NÃO Enviada!</h1> <p>Tente novamente em breve.</p>",
+function displaySuccess(event) {
+    event.innerHTML = "<h1 class='title enviado'>Mensagem Enviada!</h1> <p>Fico feliz com o seu contato e retornarei em breve.</p>";
+};
+
+function displayError(event) {
+    event.innerHTML = "<h1 class='title enviado'>Mensagem NÃO Enviada!</h1> <p>Tente novamente em breve.</p>";
+};
+
+function sendMail() {
+    let parms = {
+        name    : document.getElementById('name').value,
+        email   : document.getElementById('email').value,
+        message : document.getElementById('message').value,
+    };
+
+    emailjs.send("service_76ydzkj", "template_cy66ytr", parms)
+    .then(
+        (response) => {
+            displaySuccess(form);
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            displayError(form)
+            console.log('FAILED...', error);
+        },
+    );
+};
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    onSubmission(submitBtn);
+    sendMail();
 });
-formSubmit.init();
